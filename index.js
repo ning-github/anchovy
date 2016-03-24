55cbfe1111c595b331cc4f684a2deec9ec739bbe
Ning Xia <nxiamail@gmail.com>
2016-03-24 16:21:48 -0700

    Enter authorization by generation function prompts, make POST API call

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --harmony
+
 var program = require("commander");
+var co = require("co");
+var prompt = require("co-prompt");
 var request = require("superagent");
 
-// api info: https://developer.github.com/v3/repos/
+// api endpoint to create new GitHub repo -- api info: https://developer.github.com/v3/repos/
 var apiURL = "https://api.github.com" + "/user/repos";
 
 program
     .action(function(){
         var args = Array.prototype.slice.call(arguments);
-        var name = args.slice(0, args.length-1).join(' ');
-        console.log('what was name: ', name);
+        var name = args.slice(0, args.length-1).join(" ");
+        co(function* (){
+            var username = yield prompt("Enter GitHub username? ");
+            var password = yield prompt.password("Enter GitHub password: ");
+            request
+                .post(apiURL)
+                .auth(username, password)
+                .send({name: name})
+                .set("Accept", "application/json")
+                .end(function (err, res){
+                    console.log("Created repo at: ", res.body.url);
+                });
+        });
     })
     .parse(process.argv);
