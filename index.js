5c55dddec1ace792d7ca47d4f27b7aacd9e6073b
Ning Xia <nxiamail@gmail.com>
2016-03-24 10:35:54 -0700

    Capture passed-in name, even if it is multiple words

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
-console.log('hello world');
+var program = require("commander");
+var request = require("superagent");
 
 // api info: https://developer.github.com/v3/repos/
-var apiURL = "https://api.github.com";
+var apiURL = "https://api.github.com" + "/user/repos";
+
+program
+    .action(function(){
+        var args = Array.prototype.slice.call(arguments);
+        var name = args.slice(0, args.length-1).join(' ');
+        console.log('what was name: ', name);
+    })
+    .parse(process.argv);
